d687ee5b899f50a620462624144eccee0801595b
J511Y <47891005+J511Y@users.noreply.github.com>
2020-10-21 08:47:39 +0900

    5강

--- pashion/views.py
+++ pashion/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from .models import User
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html");
+    users = User.objects.all()
+    return render(request, "home.html", {'users' : users})
+
+def profile(request, id):
+    user = get_object_or_404(User, pk = id)
+    return render(request, 'profile.html', {'user' : user})
