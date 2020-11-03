c0ee8eb7a289af0338bd4ddbff31724848d3c76b
J511Y <47891005+J511Y@users.noreply.github.com>
2020-11-03 12:00:51 +0900

    util, common 생성 / 데코레이터 적용 / 게시글 작성 간이로 작성

--- pashion/views.py
+++ pashion/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, get_object_or_404
+from daily.models import *
+from daily.forms import *
+
+from user.models import *
+from user.forms import *
 
 # Create your views here.
 
 
 def main(request):
-    return render(request, "main.html")
+    data = {}
+    data["Daily"] = Daily.objects.all()
+    data["DailyForm"] = DailyForm()
+    return render(request, "main.html", data)
 
 
 def profile(request):
