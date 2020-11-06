ab6ae83d4fe969097b5c4de784f7b103173383e6
Dayeaa <de-da@naver.com>
2020-11-06 19:29:13 +0900

    commit

--- user/urls.py
+++ user/urls.py
@@ -8,11 +8,7 @@ urlpatterns = [
     path('regist/', Regist.as_view(), name="regist"),
     path('login/', Login.as_view(), name="login"),
     path('logout/', Logout.as_view(), name="logout"),
-<<<<<<< HEAD
     path('profile/', Profile.as_view(), name="profile"),
     path('profile_update/', Profile_update.as_view(), name="profile_update")
 
-=======
-    path('profile/', Profile.as_view(), name="profile")
->>>>>>> c3746f84a0ef2ce485e8eaf5bdef8643aff5a323
 ]
