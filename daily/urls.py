936b4c5b06a1cd12fb1bfa758a60ec94e23e9c6c
J511Y <rpdla1456@gmail.com>
2020-11-06 15:58:22 +0900

    daily action url 변경

--- daily/urls.py
+++ daily/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     #path('', views.index, name='index'),
     path('<int:daily_id>/', DailyDetail.as_view(), name="detail"),
     path('upload/', DailyDetail.as_view(), name="upload"),
-    path('like/', UserToDailyAction.as_view(), name="daily_like_action"),
-    path('bookmark/', UserToDailyAction.as_view(), name="daily_bookmark_action"),
+    path('action/', UserToDailyAction.as_view(), name="action")
 ]
