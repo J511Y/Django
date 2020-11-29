b5d45b60fced014701e39a2f6711d0f01db5691f
J511Y <47891005+J511Y@users.noreply.github.com>
2020-11-30 02:42:32 +0900

    댓글 일부 수정

--- daily/views.py
+++ daily/views.py
@@ -20,8 +20,10 @@ class DailyDetail(View):
             return ErrorMsg("존재하지 않는 게시글입니다.", "main.html")
 
         daily = Daily.objects.get(id=daily_id)
-        reply = DailyReply.objects.filter(daily_id=daily_id).order_by('-create_day')
+        reply = DailyReply.objects.filter(
+            daily_id=daily_id).order_by('ref', 'ref_num')
         return render(request, 'daily/post.html', {'daily': daily, 'reply': reply})
+
 
 class DailyUpload(View):
     @LoginAuth
@@ -40,6 +42,7 @@ class DailyUpload(View):
         return render(request, 'daily/upload.html', {'form': form})
 
 # ajax 요청
+
 
 class UserToDailyAction(View):
     def post(self, request):
@@ -70,10 +73,12 @@ class UserToDailyAction(View):
         count = DailyLike.objects.filter(daily_id=data['daily_id']).count()
         return HttpResponse("|||SUCCESS|||" + str(count))
 
+
 class UserToDailyLike(View):
     @LoginAuth
     def get(self, request):
         return render(request, 'daily/like.html')
+
 
 class DailyDetailDelete(View):
     def get(self, request, id):
@@ -81,30 +86,33 @@ class DailyDetailDelete(View):
         daily.delete()
         return redirect("/")
 
+
 class ReplyCreate(View):
     @LoginAuth
     def post(self, request, id):
         data = request.POST
         if 'ref' in data and data['ref']:
-            ref_num = DailyReply.objects.filter(daily_id=id, ref=data['ref']).count() + 1
+            ref_num = DailyReply.objects.filter(
+                daily_id=id, ref=data['ref']).count() + 1
             ref = data['ref']
         else:
             ref_num = 1
             ref = DailyReply.objects.filter(daily_id=id).count() + 1
 
         reply = DailyReply(
-            daily_id = Daily.objects.get(id=id),
-            content = data['content'],
-            user_id = User.objects.get(id=request.session.get('login_id')),
-            ref = ref,
-            ref_num = ref_num
+            daily_id=Daily.objects.get(id=id),
+            content=data['content'],
+            user_id=User.objects.get(id=request.session.get('login_id')),
+            ref=ref,
+            ref_num=ref_num
         )
 
         reply.save()
         return redirect("/daily/" + str(id))
 
+
 class ReplyDelete(View):
-     def get(self, request, id):
+    def get(self, request, id):
         reply = DailyReply.objects.get(pk=id)
         reply.delete()
         return redirect("/daily/" + str(reply.daily_id.id))
